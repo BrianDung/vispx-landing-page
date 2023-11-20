408e6d2a2ffc45bf657a2c51ecca6b7633eee9ae
johnnydepp9 <johnnydepp.usdt@gmail.com>
2023-11-20 11:28:54 +0700

    feat: bottom

--- src/pages/home/sections/OurService/index.tsx
+++ src/pages/home/sections/OurService/index.tsx
@@ -1,7 +1,10 @@
 import { Service } from 'src/assets/icons';
 import './styles.scss';
+import { useState } from 'react';
 
 const OurService: React.FC = () => {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className="ourservice-section">
       <div className="layout">
@@ -10,7 +13,27 @@ const OurService: React.FC = () => {
           VispX offers extensive end to end solutions for blockchain gaming projects with its
           tailored services for project needs.
         </div>
-        <img src={Service}  alt='banner'/>
+        <div className="images">
+          <img
+            src={Service}
+            alt="banner"
+            className="pointer full-width"
+            onMouseEnter={(e) => {
+              console.log('move', { e });
+              setIsShown(true);
+            }}
+            onMouseLeave={() => setIsShown(false)}
+          />
+          {isShown && (
+            <div className="content-show">
+              <div className="title-show">Capital Raise</div>
+              <div className="description-show">
+                Our partnership program raises capital on behalf of projects to give them a
+                kickstart. Capital is raised through token and game asset sales.
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
